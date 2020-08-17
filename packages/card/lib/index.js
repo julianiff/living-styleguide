4eadbf9767775b714aa4112678a6ce28a49fccfa
julianiff <jule_iff@mac.com>
2020-08-17 17:31:03 +0200

    Changed dumytext of component

--- packages/card/lib/index.js
+++ packages/card/lib/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Button = ({ onClick, children }) => {
- return <button onClick={onClick}>im a card but also a button{children}</button>;
+ return <button onClick={onClick}>im a card but is called a button{children}</button>;
 };
 
 export default Button;
