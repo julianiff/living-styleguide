72103da913c78ffc4fbb644c9be75db8e99c9356
julianiff <jule_iff@mac.com>
2020-08-19 09:23:32 +0200

    fix: Changed publish.yml

--- packages/card/lib/index.js
+++ packages/card/lib/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Button = ({ onClick, children }) => {
- return <button onClick={onClick}>im a card test but test is called a button{children}</button>;
+  return <button onClick={onClick}>new Button with - {children}</button>;
 };
 
 export default Button;
