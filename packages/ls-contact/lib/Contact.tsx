63de8f6543f4c197f3e35a87b061a50fb7ece46d
flavio.keller <flavio.keller@helsana.ch>
2020-09-12 13:54:17 +0200

    feat: first draft of ls-list-image

--- packages/ls-contact/lib/Contact.tsx
+++ packages/ls-contact/lib/Contact.tsx
@@ -1,19 +1,30 @@
 import React from "react";
-import styled from "styled-components";
+import {H2} from "../../ls-styles/LsStyles"
+import {Grid} from "@material-ui/core";
 
 export interface ContactProps {
-  /**
-   * What background color to use
-   */
-  backgroundColor?: string;
+    /**
+     * What background color to use
+     */
+    backgroundColor?: string;
 }
 
 /**
  * Primary UI component for user interaction
  */
-export const Contact: React.FC<ContactProps> = ({
-  backgroundColor,
-  ...props
-}) => {
-  return (<div>Contact Test</div>);
-};
+export const Contact: React.FC<ContactProps> =
+    ({
+         backgroundColor,
+         ...props
+     }) => {
+        return (
+            <Grid container spacing={2}>
+                <Grid item xs={6}>
+                    Titlestyled
+                    Titlenormal
+                </Grid>
+                <Grid item xs={6}>
+                    Contact Test
+                </Grid>
+            </Grid>);
+    };
--- packages/ls-list-image/lib/ListImage.tsx
+++ packages/ls-list-image/lib/ListImage.tsx
@@ -1,24 +1,90 @@
 import React from "react";
 import styled from "styled-components";
+import {Grid, GridList, GridListTile, GridListTileBar, IconButton} from "@material-ui/core";
+import InfoIcon from '@material-ui/icons/Info';
+import image1 from '../../assets/Chadieja.jpg';
+import image2 from '../../assets/Ueli.jpg';
+import image3 from '../../assets/portraitcscv.jpg';
+import image4 from '../../assets/Rosy.jpg';
+import image5 from '../../assets/Sam.jpg';
 
+const dogData = [
+    {
+        img: image1,
+        title: 'Chadieja Benredjem',
+        author: 'TPA',
+        cols: 2
+    },
+    {
+        img: image2,
+        title: 'Dr. med. vet. Ulrich Coradi',
+        author: 'Arzt',
+        cols: 2
+    },
+    {
+        img: image3,
+        title: 'Dr. med. vet. Corinne Schroeter',
+        author: 'Praxisinhaberin',
+        cols: 2
+    },
+    {
+        img: image4,
+        title: 'Rosy',
+        author: 'Praxishund',
+        cols: 3
+    },
+    {
+        img: image5,
+        title: 'Sam',
+        author: 'Praxishund',
+        cols: 3
+    },
+];
+
+/**
+ * The example data is structured as follows:
+ * [etc...]
+ *
+ */
 export interface ListImageProps {
-  /**
-   * What background color to use
-   */
-  backgroundColor?: string;
+    /**
+     * What background color to use
+     */
+    backgroundColor?: string;
+    classes: any;
+    tileData: any;
 }
 
-const BgColor = styled.button`
-  background-color: yellow !important;
-`;
+
 /**
  * Primary UI component for user interaction
  */
 export const ListImage: React.FC<ListImageProps> = ({
-  backgroundColor,
-  ...props
-}) => {
-  return (
-      <div>ListImage</div>
-  );
+                                                        backgroundColor,
+                                                        classes = {gridList: "background-color: green;"},
+                                                        tileData = dogData,
+                                                        ...props
+                                                    }) => {
+    return (
+        <Grid container>
+            <Grid item>
+                <GridList cellHeight={500} className={classes.gridList} cols={6}>
+                    {tileData.map((tile: any) => (
+                        <GridListTile key={tile.img} cols={tile.cols || 1}>
+                            <img src={tile.img} alt={tile.title} style={{objectFit: "cover"}}/>
+                            <GridListTileBar
+                                title={tile.title}
+                                subtitle={tile.author}
+                                actionIcon={
+                                    <IconButton aria-label={`info about ${tile.title}`} className={classes.icon}>
+                                        <InfoIcon />
+                                    </IconButton>
+                                }
+                            />
+                        </GridListTile>
+                    ))}
+                </GridList>
+            </Grid>
+        </Grid>
+    );
 };
